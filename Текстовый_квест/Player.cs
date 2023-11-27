02a2f3d021768b3058f016e1e3051513c9a4cc34
Imperator-chebupel <138660337+Imperator-chebupel@users.noreply.github.com>
2023-11-27 20:09:18 +0700

    Заакладка основы игры
    Здесь я накидал примерный образ и схему работы текстового квеста

---
+++ Текстовый_квест/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Текстовый_квест
+{
+    internal class Player
+    {
+        public
+        int time_h = 7;
+        public
+        int time_m = 0;
+        float time_speed = 1.0f;
+        List <Objects> inventoty = new List<Objects>();
+        string[][] dialogs = new string[][] { };
+    }
+}
---
+++ Текстовый_квест/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace Текстовый_квест
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            while (true) 
+            {
+                Console.WriteLine("Вы готовы начать? Тогда напишите 'Да'");
+                string plot = Console.ReadLine();
+                if (plot == "Да")
+                    break;
+                Console.WriteLine("Ясно, тогда попозже");
+            }
+            Console.Clear();
+            Console.WriteLine(player.time_h + ":" + player.time_m.ToString("00"));
+            Console.WriteLine("Вы просыпаетесь в своей комнате и понимаете, что сегодня экзамен, к которому Вы не готовы. Что делать?");
+            Console.WriteLine("Для сдачи экзамена нужно собрать несколько предметов: канцелярию, пропуск в корпус и несколько конспектов по дисциплине. Ограничение по времени - до 8:30");
+        }
+    }
+}
