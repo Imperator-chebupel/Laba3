457963cdb57e1041c3b9197195a974a273a2fb92
agsrexby <145462310+agsrexby@users.noreply.github.com>
2023-12-02 20:11:27 +0700

    Редактирование системы предметов
    были добавлены используемые и не используемые предметы

---
+++ Текстовый_квест_с_интерфейсом/Item_usefull.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Текстовый_квест_с_интерфейсом
+{
+    public class Item_usefull : Item
+    {
+        public override string Write()
+        {
+            return (Name_ + "\r\n " + Discription_ + "\r\n" + "Необходимый для зачёта предмет");
+        }
+    }
+}
---
+++ Текстовый_квест_с_интерфейсом/Item_useless.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Текстовый_квест_с_интерфейсом
+{
+    public class Item_useless : Item
+    {
+        public override string Write()
+        {
+            return (Name_ + "\r\n" + Discription_ + "\r\n" + "Никто не знает, понадобится ли эта штука на экзамене...");
+        }
+    }
+}
