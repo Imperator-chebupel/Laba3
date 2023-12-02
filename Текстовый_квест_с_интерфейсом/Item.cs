6deb741f2c4e74f1a6ddf64df4d5db6dea835f9c
agsrexby <145462310+agsrexby@users.noreply.github.com>
2023-12-02 20:04:54 +0700

    Введение системы предметов
    тут я попытался добавить предметы

---
+++ Текстовый_квест_с_интерфейсом/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Текстовый_квест_с_интерфейсом
+{
+    public abstract class Item
+    {
+        string Name;
+        string Discription;
+
+        public string Name_
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public string Discription_ 
+        {
+            get { return Discription; } 
+            set { Discription = value; } 
+        }
+
+        public abstract string Write();
+        
+    }
+}
